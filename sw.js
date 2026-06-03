e114ba44ca42e25b4cb11d212b3636307de6f694
Ravn00 <130533058+Ravn00@users.noreply.github.com>
2026-06-03 01:09:21 -0400

    fix: bump cache panel-v3 to panel-v4 por cambios en index.html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'panel-v3';
+const CACHE = 'panel-v4';
 const FILES = ['./','./index.html','./manifest-panel.json','./icon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))).then(() => self.clients.claim())); });
