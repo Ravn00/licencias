ceedf1708690cdf2fff66b50a901a1efbbb360b9
Ravn00 <130533058+Ravn00@users.noreply.github.com>
2026-06-06 16:53:36 -0400

    [PWA] Cachear JS faltantes

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE = 'panel-v4';
-const FILES = ['./','./index.html','./manifest-panel.json','./icon.png'];
+const CACHE = 'acap-v1';
+const FILES = ['./','./index.html','./manifest-panel.json','./icon.png',
+  'css/styles.css',
+  'js/config.js','js/supabase.js','js/constants.js',
+  'js/license.js','js/init.js','js/ui.js','js/health.js','js/app.js'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
